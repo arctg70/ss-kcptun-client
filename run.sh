d13a974531e85509b9caa8809a8d77cfa354d6c1
arctg70 <simon.zhou@gmail.com>
2017-07-28 00:54:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -62,12 +62,14 @@ fi
     "nocomp": false
 }
 EOF
-echo "Lighttpd badbados is running..." > /var/www/index.html
+# echo "Lighttpd badbados is running..." > /var/www/index.html
 env | grep -E '^MARATHON_HOST=|MARATHON_PORT_' >> /var/www/index.html
 if [ "x$MARATHON_HOST" != "x" ]; then
     getent hosts $MARATHON_HOST | awk '{print "MARATHON_HOST_IP="$1; exit;}' >> /var/www/index.html
 fi
 env | grep -E '^MARATHON|MESOS' >>  /var/www/index.html
+echo SS_PASSWORD=${SS_PASSWORD} >>  /var/www/index.html
+echo SS_METHOD=${SS_METHOD} >>  /var/www/index.html
 # rc-service lighttpd start
 echo "root:${ROOT_PASSWORD}" | chpasswd > /dev/null 2>&1
 /usr/sbin/sshd -o PermitRootLogin=yes -o UseDNS=no
